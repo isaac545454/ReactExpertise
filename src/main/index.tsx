d19e58db9c2cd8398f884d3d79f2ebd0d1125199
Isaac1221 <isaac.newton1221@gmail.com>
2023-10-12 11:19:18 -0300

    vite-env

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './style/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <div />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<div />
+	</React.StrictMode>,
 )
